1c3946f088a53682b8f015acebd56ffa66248ca8
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2024-05-18 03:39:30 +0000

    Add pdf to text exercice

---
+++ 4_pdf_to_text.py
@@ -0,0 +1,28 @@
+"""Ce script permet d'extraire le contenu textuel d'un fichier PDF"""
+from pypdf import PdfReader
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python pdf_to_text.py path_to_file.pdf")
+
+filename = sys.argv[1]
+
+try:
+
+    reader = PdfReader(filename)
+    number_of_pages = len(reader.pages)
+
+    text_content = []
+    for page in reader.pages:
+        text = page.extract_text()
+        text_content.append(text)
+
+    resultfile = filename + '.txt'
+    with open(resultfile, 'w', encoding='utf-8') as file:
+        file.writelines(text_content)
+
+    print('Extraction terminée !')
+    print(f'Résultat dans le fichier "{resultfile}"')
+
+except FileNotFoundError:
+    sys.exit("Error: fichier introuvable !")
